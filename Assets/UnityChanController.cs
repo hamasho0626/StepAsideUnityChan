4294cb4313b4ddcf0b1473fede3670f2d8ce1a88
hamasho0626 <kurono235@gmail.com>
2018-09-18 23:02:51 +0900

    Scene作成 ユニティちゃんに走るアニメーションを追加したところまで作成

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour {
+
+    //アニメーションするためのコンポーネントを入れる
+    private Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+        //Animatorコンポーネントを取得
+        this.myAnimator = GetComponent<Animator>();
+
+        //走るアニメーションを開始
+        this.myAnimator.SetFloat("Speed", 1);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
