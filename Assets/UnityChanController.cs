2eacb56b148e5cecccf246cb4c7f93f0f8bb7aa6
hamasho0626 <kurono235@gmail.com>
2018-09-19 09:04:08 +0900

    Unityちゃんに左右の移動とジャンプの処理を追加

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -6,10 +6,16 @@ public class UnityChanController : MonoBehaviour {
 
     //アニメーションするためのコンポーネントを入れる
     private Animator myAnimator;
-    //Unityちゃんを移動させるコンポーネントを入れる（追加）
+    //Unityちゃんを移動させるコンポーネントを入れる
     private Rigidbody myRigidbody;
-    //前進するための力（追加）
+    //前進するための力
     private float forwardforce = 800.0f;
+    //左右に移動するための力
+    private float turnForce = 500.0f;
+    //ジャンプするための力
+    private float upForce = 500.0f;
+    //左右の移動できる範囲
+    private float movableRange = 3.4f;
 
 	// Use this for initialization
 	void Start () {
@@ -26,6 +32,32 @@ public class UnityChanController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //ユニティちゃんに前方向の力を加える
         this.myRigidbody.AddForce(this.transform.forward * this.forwardforce);
+
+        //Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる
+        if(Input.GetKey(KeyCode.LeftArrow) && - this.movableRange < this.transform.position.x)
+        {
+            //左に移動
+            this.myRigidbody.AddForce(-this.turnForce, 0, 0);
+        } else if(Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < this.movableRange)
+        {
+            //右に移動
+            this.myRigidbody.AddForce(this.turnForce, 0, 0);
+        }
+        //Jumpステートの場合はJumpにfalseをセットする
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
+        }
+
+        //ジャンプしていないときにスペースが押されたらジャンプする
+        if(Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f)
+        {
+            //ジャンプアニメを再生
+            this.myAnimator.SetBool("Jump", true);
+            //Unityちゃんに上向きの方向の力を加える
+            this.myRigidbody.AddForce(this.transform.up * this.upForce);
+        }
 	}
 }
